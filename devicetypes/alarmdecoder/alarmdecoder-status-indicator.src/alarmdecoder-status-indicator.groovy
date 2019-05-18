6a3d829fe61e6ca397a72b04c5a58e4b3058924b
f34rdotcom <coder@f34r.com>
2019-05-18 13:20:44 -0700

    tidy.

--- devicetypes/alarmdecoder/alarmdecoder-status-indicator.src/alarmdecoder-status-indicator.groovy
+++ devicetypes/alarmdecoder/alarmdecoder-status-indicator.src/alarmdecoder-status-indicator.groovy
@@ -1,22 +1,22 @@
 /**
-*  Virtual status indicator to show a specific alarm status
-*
-*  Copyright 2016-2018 Nu Tech Software Solutions, Inc.
-*
-*  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License. You may obtain a copy of the License at:
-*
-*      http://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
-*  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
-*  for the specific language governing permissions and limitations under the License.
-*
-*/
+ *  Virtual status indicator to show a specific alarm status
+ *
+ *  Copyright 2016-2018 Nu Tech Software Solutions, Inc.
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ *
+ */
 
 /*
-* global support
-*/
+ * global support
+ */
 import groovy.transform.Field
 @Field APPNAMESPACE = "alarmdecoder"
 
